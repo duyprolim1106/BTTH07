ddf5a4095fe48abd157ee85112faef33ebf93cb9
kimbau910 <2051063478@e.tlu.edu.vn>
2022-03-24 20:17:16 +0700

    Bau da them

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// let txtCheckName = document.getElementById('txtName')
+
+// let inputName = /^[A-Za-z_-]{3,16}$/
+
+// function checkName(){
+//     if(inputName.test(txtCheckName.value)){
+        
+//     }
+// }
